f304813af9c476e2c099ac848e2200a6d870a105
mico28 <skrbinekmitja@gmail.com>
2019-01-04 00:19:11 +0100

    1

---
+++ app/Http/Controllers/SirinaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sirina;
+use Illuminate\Http\Request;
+
+class SirinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sirina  $sirina
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sirina $sirina)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sirina  $sirina
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sirina $sirina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sirina  $sirina
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sirina $sirina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sirina  $sirina
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sirina $sirina)
+    {
+        //
+    }
+}
